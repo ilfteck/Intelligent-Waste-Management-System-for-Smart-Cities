c0fd09dafb6d8a603925e7d824f9d11428435967
ilfteck <116196658+ilfteck@users.noreply.github.com>
2024-05-03 18:32:49 +0800

    Update DictionaryModel.java

--- code/src/main/java/com/entity/model/DictionaryModel.java
+++ code/src/main/java/com/entity/model/DictionaryModel.java
@@ -10,10 +10,10 @@ import java.io.Serializable;
 
 
 /**
- * 字典
- * 接收传参的实体类
- *（实际开发中配合移动端接口开发手动去掉些没用的字段， 后端一般用entity就够用了）
- * 取自ModelAndView 的model名称
+ * dictionary
+ * The entity class that receives the argument
+ *(In the actual development, with the mobile interface development, manually remove some useless fields, and the backend is generally enough to use entity)
+ * model name from ModelAndView
  */
 public class DictionaryModel implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -22,164 +22,130 @@ public class DictionaryModel implements Serializable {
 
 
     /**
-     * 主键
+     * id
      */
     private Long id;
 
 
     /**
-     * 字段
+     * dicCode
      */
     private String dicCode;
 
 
     /**
-     * 字段名
+     * dicName
      */
     private String dicName;
 
 
     /**
-     * 编码
+     * codeIndex
      */
     private Integer codeIndex;
 
 
     /**
-     * 编码名字
+     * codeIndex
      */
-    private String indexName;
+    private String codeIndex;
 
 
     /**
-     * 父字段id
+     * superId
      */
     private Integer superId;
 
 
     /**
-     * 备注
+     * beizhu
      */
     private String beizhu;
 
 
     /**
-     * 创建时间
+     * createTime
      */
     @JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd HH:mm:ss")
 	@DateTimeFormat
     private Date createTime;
 
 
-    /**
-	 * 获取：主键
-	 */
+  
     public Long getId() {
         return id;
     }
 
 
-    /**
-	 * 设置：主键
-	 */
+    
     public void setId(Long id) {
         this.id = id;
     }
-    /**
-	 * 获取：字段
-	 */
+   
     public String getDicCode() {
         return dicCode;
     }
 
 
-    /**
-	 * 设置：字段
-	 */
+   
     public void setDicCode(String dicCode) {
         this.dicCode = dicCode;
     }
-    /**
-	 * 获取：字段名
-	 */
+    
     public String getDicName() {
         return dicName;
     }
 
 
-    /**
-	 * 设置：字段名
-	 */
+  
     public void setDicName(String dicName) {
         this.dicName = dicName;
     }
-    /**
-	 * 获取：编码
-	 */
+    
     public Integer getCodeIndex() {
         return codeIndex;
     }
 
 
-    /**
-	 * 设置：编码
-	 */
+  
     public void setCodeIndex(Integer codeIndex) {
         this.codeIndex = codeIndex;
     }
-    /**
-	 * 获取：编码名字
-	 */
+   
     public String getIndexName() {
         return indexName;
     }
 
 
-    /**
-	 * 设置：编码名字
-	 */
+    
     public void setIndexName(String indexName) {
         this.indexName = indexName;
     }
-    /**
-	 * 获取：父字段id
-	 */
+  
     public Integer getSuperId() {
         return superId;
     }
 
 
-    /**
-	 * 设置：父字段id
-	 */
     public void setSuperId(Integer superId) {
         this.superId = superId;
     }
-    /**
-	 * 获取：备注
-	 */
+   
     public String getBeizhu() {
         return beizhu;
     }
 
 
-    /**
-	 * 设置：备注
-	 */
     public void setBeizhu(String beizhu) {
         this.beizhu = beizhu;
     }
-    /**
-	 * 获取：创建时间
-	 */
+  
     public Date getCreateTime() {
         return createTime;
     }
 
 
-    /**
-	 * 设置：创建时间
-	 */
+    
     public void setCreateTime(Date createTime) {
         this.createTime = createTime;
     }
