ff746c8037beb43053075e737d7bdcbc3e34a3c3
ilfteck <116196658+ilfteck@users.noreply.github.com>
2024-05-03 18:50:30 +0800

    Update AnnouncementEntity.java

--- code/src/main/java/com/entity/AnnouncementEntity.java
+++ code/src/main/java/com/entity/AnnouncementEntity.java
@@ -18,7 +18,7 @@ import com.utils.DateUtil;
 
 
 /**
- * 公告
+ * announcement
  *
  * @author 
  * @email
@@ -43,160 +43,131 @@ public class AnnouncementEntity<T> implements Serializable {
 
 
     /**
-     * 主键
+     * ID
      */
     @TableId(type = IdType.AUTO)
-    @ColumnInfo(comment="主键",type="int(11)")
+    @ColumnInfo(comment="id",type="int(11)")
     @TableField(value = "id")
 
     private Integer id;
 
 
     /**
-     * 公告名称
+     * announcement_name
      */
-    @ColumnInfo(comment="公告名称",type="varchar(200)")
+    @ColumnInfo(comment="announcement_name",type="varchar(200)")
     @TableField(value = "announcement_name")
 
     private String announcementName;
 
 
     /**
-     * 公告图片
+     * announcement_photo
      */
-    @ColumnInfo(comment="公告图片",type="varchar(200)")
+    @ColumnInfo(comment="announcement_photo",type="varchar(200)")
     @TableField(value = "announcement_photo")
 
     private String announcementPhoto;
 
 
     /**
-     * 公告类型
+     * announcement_types
      */
-    @ColumnInfo(comment="公告类型",type="int(11)")
+    @ColumnInfo(comment="announcement_types",type="int(11)")
     @TableField(value = "announcement_types")
 
     private Integer announcementTypes;
 
 
     /**
-     * 公告发布时间
+     * insert_time
      */
     @JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd HH:mm:ss")
 	@DateTimeFormat
-    @ColumnInfo(comment="公告发布时间",type="timestamp")
+    @ColumnInfo(comment="insert_time",type="timestamp")
     @TableField(value = "insert_time",fill = FieldFill.INSERT)
 
     private Date insertTime;
 
 
     /**
-     * 公告详情
+     * announcement_content
      */
-    @ColumnInfo(comment="公告详情",type="longtext")
+    @ColumnInfo(comment="announcement_content",type="longtext")
     @TableField(value = "announcement_content")
 
     private String announcementContent;
 
 
     /**
-     * 创建时间
+     * create_time
      */
     @JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd HH:mm:ss")
 	@DateTimeFormat
-    @ColumnInfo(comment="创建时间",type="timestamp")
+    @ColumnInfo(comment="create_time",type="timestamp")
     @TableField(value = "create_time",fill = FieldFill.INSERT)
 
     private Date createTime;
 
 
-    /**
-	 * 获取：主键
-	 */
     public Integer getId() {
         return id;
     }
-    /**
-	 * 设置：主键
-	 */
+ 
 
     public void setId(Integer id) {
         this.id = id;
     }
-    /**
-	 * 获取：公告名称
-	 */
+ 
     public String getannouncementName() {
         return announcementName;
     }
-    /**
-	 * 设置：公告名称
-	 */
+
 
     public void setannouncementName(String announcementName) {
         this.announcementName = announcementName;
     }
-    /**
-	 * 获取：公告图片
-	 */
+  
     public String getannouncementPhoto() {
         return announcementPhoto;
     }
-    /**
-	 * 设置：公告图片
-	 */
+  
 
     public void setannouncementPhoto(String announcementPhoto) {
         this.announcementPhoto = announcementPhoto;
     }
-    /**
-	 * 获取：公告类型
-	 */
+
     public Integer getannouncementTypes() {
         return announcementTypes;
     }
-    /**
-	 * 设置：公告类型
-	 */
+ 
 
     public void setannouncementTypes(Integer announcementTypes) {
         this.announcementTypes = announcementTypes;
     }
-    /**
-	 * 获取：公告发布时间
-	 */
+
     public Date getInsertTime() {
         return insertTime;
     }
-    /**
-	 * 设置：公告发布时间
-	 */
+
 
     public void setInsertTime(Date insertTime) {
         this.insertTime = insertTime;
     }
-    /**
-	 * 获取：公告详情
-	 */
+ 
     public String getannouncementContent() {
         return announcementContent;
     }
-    /**
-	 * 设置：公告详情
-	 */
+
 
     public void setannouncementContent(String announcementContent) {
         this.announcementContent = announcementContent;
     }
-    /**
-	 * 获取：创建时间
-	 */
+ 
     public Date getCreateTime() {
         return createTime;
     }
-    /**
-	 * 设置：创建时间
-	 */
+ 
 
     public void setCreateTime(Date createTime) {
         this.createTime = createTime;
