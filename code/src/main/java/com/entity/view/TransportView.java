b0763ad048cf1b4cbef67542999b5716699eae43
ilfteck <116196658+ilfteck@users.noreply.github.com>
2024-05-03 18:48:57 +0800

    Update TransportView.java

--- code/src/main/java/com/entity/view/TransportView.java
+++ code/src/main/java/com/entity/view/TransportView.java
@@ -12,69 +12,69 @@ import java.util.Date;
 import com.utils.DateUtil;
 
 /**
-* 运输
-* 后端返回视图实体辅助类
-* （通常后端关联的表或者自定义的字段需要返回使用）
+* Transportation
+* The back end returns the view entity helper class
+* (Usually back-end associated tables or custom fields need to be returned for use)
 */
 @TableName("transport")
 public class TransportView extends TransportEntity implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	//当前表
+	
 	/**
-	* 运输类型的值
+	* The value of the transportation type
 	*/
-	@ColumnInfo(comment="运输类型的字典表值",type="varchar(200)")
+	@ColumnInfo(comment="Dictionary table value for the transport type",type="varchar(200)")
 	private String transportValue;
 
-	//级联表 垃圾出库申请
+	
 		/**
-		* 报名编号
+		* Registration number
 		*/
 
-		@ColumnInfo(comment="报名编号",type="varchar(200)")
+		@ColumnInfo(comment="recycleYuyueUuidNumbe",type="varchar(200)")
 		private String recycleYuyueUuidNumber;
 										 
 		/**
-		* 垃圾出库申请 的 用户
+		*Garbage daelivery application user
 		*/
-		@ColumnInfo(comment="用户",type="int(11)")
+		@ColumnInfo(comment="User",type="int(11)")
 		private Integer recycleYuyuememberId;
 		/**
-		* 报名理由
+		*recycleYuyueText
 		*/
 
-		@ColumnInfo(comment="报名理由",type="longtext")
+		@ColumnInfo(comment="recycleYuyueText",type="longtext")
 		private String recycleYuyueText;
 		/**
-		* 出库时间
+		* recycletransportTime
 		*/
 		@JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd HH:mm:ss")
 		@DateTimeFormat
-		@ColumnInfo(comment="出库时间",type="timestamp")
+		@ColumnInfo(comment="recycletransportTime",type="timestamp")
 		private Date recycletransportTime;
 		/**
-		* 申请状态
+		* recycleYuyueYesnoTypes
 		*/
-		@ColumnInfo(comment="申请状态",type="int(11)")
+		@ColumnInfo(comment="recycleYuyueYesnoTypes",type="int(11)")
 		private Integer recycleYuyueYesnoTypes;
 			/**
-			* 申请状态的值
+			* recycleYuyueYesnoValue
 			*/
-			@ColumnInfo(comment="申请状态的字典表值",type="varchar(200)")
+			@ColumnInfo(comment="recycleYuyueYesnoValue",type="varchar(200)")
 			private String recycleYuyueYesnoValue;
 		/**
-		* 审核回复
+		* recycleYuyueYesnoText
 		*/
 
-		@ColumnInfo(comment="审核回复",type="longtext")
+		@ColumnInfo(comment="recycleYuyueYesnoText",type="longtext")
 		private String recycleYuyueYesnoText;
 		/**
-		* 审核时间
+		* recycleYuyueShenheTime
 		*/
 		@JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd HH:mm:ss")
 		@DateTimeFormat
-		@ColumnInfo(comment="审核时间",type="timestamp")
+		@ColumnInfo(comment="recycleYuyueShenheTime",type="timestamp")
 		private Date recycleYuyueShenheTime;
 
 
@@ -93,123 +93,81 @@ public class TransportView extends TransportEntity implements Serializable {
 	}
 
 
-
-	//当前表的
-	/**
-	* 获取： 运输类型的值
-	*/
 	public String gettransportValue() {
 		return transportValue;
 	}
-	/**
-	* 设置： 运输类型的值
-	*/
+	
 	public void settransportValue(String transportValue) {
 		this.transportValue = transportValue;
 	}
 
 
-	//级联表的get和set 垃圾出库申请
-
-		/**
-		* 获取： 报名编号
-		*/
 		public String getrecycleYuyueUuidNumber() {
 			return recycleYuyueUuidNumber;
 		}
-		/**
-		* 设置： 报名编号
-		*/
+		
 		public void setrecycleYuyueUuidNumber(String recycleYuyueUuidNumber) {
 			this.recycleYuyueUuidNumber = recycleYuyueUuidNumber;
 		}
-		/**
-		* 获取：垃圾出库申请 的 用户
-		*/
+		
+	
 		public Integer getrecycleYuyuememberId() {
 			return recycleYuyuememberId;
 		}
-		/**
-		* 设置：垃圾出库申请 的 用户
-		*/
+		
 		public void setrecycleYuyuememberId(Integer recycleYuyuememberId) {
 			this.recycleYuyuememberId = recycleYuyuememberId;
 		}
 
-		/**
-		* 获取： 报名理由
-		*/
 		public String getrecycleYuyueText() {
 			return recycleYuyueText;
 		}
-		/**
-		* 设置： 报名理由
-		*/
+		
 		public void setrecycleYuyueText(String recycleYuyueText) {
 			this.recycleYuyueText = recycleYuyueText;
 		}
 
-		/**
-		* 获取： 出库时间
-		*/
+		
 		public Date getrecycletransportTime() {
 			return recycletransportTime;
 		}
-		/**
-		* 设置： 出库时间
-		*/
+		
 		public void setrecycletransportTime(Date recycletransportTime) {
 			this.recycletransportTime = recycletransportTime;
 		}
-		/**
-		* 获取： 申请状态
-		*/
+		
 		public Integer getrecycleYuyueYesnoTypes() {
 			return recycleYuyueYesnoTypes;
 		}
-		/**
-		* 设置： 申请状态
-		*/
+		
 		public void setrecycleYuyueYesnoTypes(Integer recycleYuyueYesnoTypes) {
 			this.recycleYuyueYesnoTypes = recycleYuyueYesnoTypes;
 		}
 
 
-			/**
-			* 获取： 申请状态的值
-			*/
+		
 			public String getrecycleYuyueYesnoValue() {
 				return recycleYuyueYesnoValue;
 			}
-			/**
-			* 设置： 申请状态的值
-			*/
+		
 			public void setrecycleYuyueYesnoValue(String recycleYuyueYesnoValue) {
 				this.recycleYuyueYesnoValue = recycleYuyueYesnoValue;
 			}
 
-		/**
-		* 获取： 审核回复
-		*/
+	
 		public String getrecycleYuyueYesnoText() {
 			return recycleYuyueYesnoText;
 		}
-		/**
-		* 设置： 审核回复
-		*/
+	
 		public void setrecycleYuyueYesnoText(String recycleYuyueYesnoText) {
 			this.recycleYuyueYesnoText = recycleYuyueYesnoText;
 		}
 
-		/**
-		* 获取： 审核时间
-		*/
+		
 		public Date getrecycleYuyueShenheTime() {
 			return recycleYuyueShenheTime;
 		}
-		/**
-		* 设置： 审核时间
-		*/
+		
 		public void setrecycleYuyueShenheTime(Date recycleYuyueShenheTime) {
 			this.recycleYuyueShenheTime = recycleYuyueShenheTime;
 		}
