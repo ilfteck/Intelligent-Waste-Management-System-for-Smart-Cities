53ffca28ac3a4bde0e3a762571ee36a41700145e
ilfteck <116196658+ilfteck@users.noreply.github.com>
2024-05-03 17:32:58 +0800

    Update LoginUser.java

--- code/src/main/java/com/annotation/LoginUser.java
+++ code/src/main/java/com/annotation/LoginUser.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 登录用户信息
+ * Login user information
  */
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
