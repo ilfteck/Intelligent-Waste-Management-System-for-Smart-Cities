491368ba64a577d99ed179a226cd44cff976ed6c
ilfteck <116196658+ilfteck@users.noreply.github.com>
2024-05-03 19:33:53 +0800

    Update Query.java

--- code/src/main/java/com/utils/Query.java
+++ code/src/main/java/com/utils/Query.java
@@ -9,25 +9,25 @@ import org.apache.commons.lang3.StringUtils;
 import com.baomidou.mybatisplus.plugins.Page;
 
 /**
- * 查询参数
+ * Query parameter
  */
 public class Query<T> extends LinkedHashMap<String, Object> {
 	private static final long serialVersionUID = 1L;
     /**
-     * mybatis-plus分页参数
+     * mybatis-plus paging parameters
      */
     private Page<T> page;
     /**
-     * 当前页码
+     * currPage
      */
     private int currPage = 1;
     /**
-     * 每页条数
+     * limit
      */
     private int limit = 10;
 
     public Query(JQPageInfo pageInfo) {
-    	//分页参数
+    	//Paging parameter
         if(pageInfo.getPage()!= null){
             currPage = pageInfo.getPage();
         }
@@ -36,15 +36,15 @@ public class Query<T> extends LinkedHashMap<String, Object> {
         }
 
     
-        //防止SQL注入（因为sidx、order是通过拼接SQL实现排序的，会有SQL注入风险）
+        //Prevent SQL injection (because sidx and order are sorted by concatenating SQL, there will be SQL injection risk)
         String sidx = SQLFilter.sqlInject(pageInfo.getSidx());
         String order = SQLFilter.sqlInject(pageInfo.getOrder());
         
 
-        //mybatis-plus分页
+        //mybatis-pluspaging
         this.page = new Page<>(currPage, limit);
 
-        //排序
+        //sort
         if(StringUtils.isNotBlank(sidx) && StringUtils.isNotBlank(order)){
             this.page.setOrderByField(sidx);
             this.page.setAsc("ASC".equalsIgnoreCase(order));
@@ -55,7 +55,7 @@ public class Query<T> extends LinkedHashMap<String, Object> {
     public Query(Map<String, Object> params){
         this.putAll(params);
 
-        //分页参数
+        //Paging parameter
         if(params.get("page") != null){
             currPage = Integer.parseInt(String.valueOf(params.get("page")));
         }
@@ -67,16 +67,16 @@ public class Query<T> extends LinkedHashMap<String, Object> {
         this.put("page", currPage);
         this.put("limit", limit);
 
-        //防止SQL注入（因为sidx、order是通过拼接SQL实现排序的，会有SQL注入风险）
+        //Prevent SQL injection (because sidx and order are sorted by concatenating SQL, there will be SQL injection risk)
         String sidx = SQLFilter.sqlInject((String)params.get("sidx"));
         String order = SQLFilter.sqlInject((String)params.get("order"));
         this.put("sidx", sidx);
         this.put("order", order);
 
-        //mybatis-plus分页
+        //mybatis-pluspaging
         this.page = new Page<>(currPage, limit);
 
-        //排序
+        //sort
         if(StringUtils.isNotBlank(sidx) && StringUtils.isNotBlank(order)){
             this.page.setOrderByField(sidx);
             this.page.setAsc("ASC".equalsIgnoreCase(order));
