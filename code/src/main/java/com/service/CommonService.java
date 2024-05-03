829ef05a13611a5a9b78ac3b2f29ce07c6a0f08f
ilfteck <116196658+ilfteck@users.noreply.github.com>
2024-05-03 19:17:30 +0800

    Update CommonService.java

--- code/src/main/java/com/service/CommonService.java
+++ code/src/main/java/com/service/CommonService.java
@@ -12,32 +12,32 @@ public interface CommonService {
 	List<Map<String, Object>> barCountTwo(Map<String, Object> params);
 
 	/**
-	 tableName 查询表
-	 condition1 条件1
-	 condition1Value 条件1值
-	 average 计算平均评分
+	 tableName 
+	 condition1 
+	 condition1Value 
+	 average 
 	 * */
 	Map<String, Object> queryScore(Map<String, Object> params);
 	/**
-	 * 新的级联字典表的  分组求和统计
+	 * newSelectGroupSum
 	 * @param params
 	 * @return
 	 */
 	List<Map<String, Object>> newSelectGroupSum(Map<String, Object> params);
 	/**
-	 * 新的级联字典表的  分组求和统计
+	 * newSelectGroupCount
 	 * @param params
 	 * @return
 	 */
 	List<Map<String, Object>> newSelectGroupCount(Map<String, Object> params);
 	/**
-	 * 柱状图求和
+	 * barSum
 	 * @param params
 	 * @return
 	 */
 	List<Map<String, Object>> barSum(Map<String, Object> params);
 	/**
-	 * 柱状图统计
+	 * barCount
 	 * @param params
 	 * @return
 	 */
